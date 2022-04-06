b816dabc74364e052ffd66a05f7c498afea1c08d
Vova <kot@gmail.com>
2022-04-06 07:27:59 +0300

    fixed the bugs

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,82 +1,72 @@
 import React, { useState } from "react";
-import API from '../API';
+import api from "../API"
 
 const Users = () => {
-   const [users, setUsers] = useState(API.users.fetchAll());
-   const [count, setCount] = useState(users.length);
+   const [users, setUsers] = useState(api.users.fetchAll());
+
+const handleDelete = (userId) => {
+   setUsers(users.filter((user) => user._id !== userId));
+};
 
    const renderPhrase = (number) => {
-      if (count === 2 || count === 3 || count === 4) {
-         return <span>Человека тусанут</span>;
-      } else {
-         return <span>Человек тусанёт</span>;
-      };
-   };
-
-   const formatCount = () => {
-      return count === 0 ? <h2>Никто с тобой не тусанёт</h2> :
-         <h2>{count} {renderPhrase()} с тобой сегодня</h2>;
-   };
-
-   const getBadgeClasses = () => {
-      let classes = 'badge ';
-      classes += count === 0 ? 'bg-danger' : 'bg-primary';
-      return classes;
-   };
-
-   const handleDecrement = (id) => {
-      setCount((prevState) => prevState - 1);
-      setUsers((prevState) => prevState.filter((user) => user._id !== id));
-   };
-
-   const tableHead = () => {
-      return <thead>
-         <tr>
-            <th>Имя</th>
-            <th>Качества</th>
-            <th>Профессия</th>
-            <th>Встретился,раз</th>
-            <th>Оценка</th>
-            <th>Удалить</th>
-         </tr>
-      </thead>
-   };
-
-   const tableBody = () => {
-      return (
-         users.map((user) => {
-            return <tr key={user._id}>
-               <td>{user.name}</td>
-               <td className={user.qualities.color}>
-                  {user.qualities.map((item) => {
-                     return <span key={item._id} className={`badge bg-${item.color}`}>{item.name}</span>
-                  })};
-               </td>
-               <td>{user.profession.name}</td>
-               <td>{user.completedMeetings}</td>
-               <td>{user.rate}/5</td>
-               <td>
-                  <button className="btn btn-danger"
-                     onClick={() => handleDecrement(user._id)}>
-                     delete
-                  </button>
-               </td>
-            </tr>
-         })
-      );
+      const lastOne = Number(number.toString().slice(-1));
+      if (number > 4 && number < 15) return "человек тусанет";
+      if ([2, 3, 4].indexOf(lastOne) >= 0) return "человека тусанут";
+      if (lastOne === 1) return "человек тусанет";
+      return "человек тусанет";
    };
 
    return (
       <>
-         <span className={getBadgeClasses()}>
-            {formatCount()}
-         </span>
-         <table className="table">
-            {tableHead()}
-            <tbody>
-               {tableBody()}
-            </tbody>
-         </table>
+         <h2>
+               <span
+                  className={"badge " + (users.length > 0 ? "bg-primary" : "bg-danger")}
+               >
+                  {users.length > 0
+                     ? `${users.length + " " + renderPhrase(users.length)} с тобой сегодня`
+                     : "Никто с тобой не тусанет"}
+               </span>
+         </h2>
+
+         {users.length > 0 && (
+               <table className="table">
+                  <thead>
+                     <tr>
+                           <th scope="col">Имя</th>
+                           <th scope="col">Качества</th>
+                           <th scope="col">Профессия</th>
+                           <th scope="col">Встретился, раз</th>
+                           <th scope="col">Оценка</th>
+                           <th />
+                     </tr>
+                  </thead>
+                  <tbody>
+                     {users.map((user) => (
+                           <tr key={user._id}>
+                           <td>{user.name}</td>
+                           <td>
+                              {user.qualities.map((item) => (
+                                 <span className={"badge m-1 bg-" + item.color} key={item._id}>
+                                       {item.name}
+                                 </span>
+                              ))}
+                           </td>
+                           <td>{user.profession.name}</td>
+                           <td>{user.completedMeetings}</td>
+                           <td>{user.rate} /5</td>
+                           <td>
+                              <button
+                                 onClick={() => handleDelete(user._id)}
+                                 className="btn btn-danger"
+                              >
+                                 delete
+                              </button>
+                           </td>
+                     </tr>
+                     ))}
+                  </tbody>
+               </table>
+         )}
       </>
    );
 };
